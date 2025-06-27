ee17ea16f4544c43c80f98034f3720dbb558e331
cindrellakoju <cindrellakoju@gmail.com>
2025-06-27 12:37:18 +0545

    Managed conflict

--- app/main.py
+++ app/main.py
@@ -21,51 +21,9 @@ app.include_router(communication.router)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "/login")
 
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-
-@app.post("/register", response_model=UserResponse)
-def register(user: UserCreate, db: Session = Depends(get_db)):
-    existing_user = db.query(User).filter((User.username == user.username) | (User.email == user.email)).first()
-    print("User trying to register:", user.dict())
-    if existing_user:
-        raise HTTPException(status_code=400, detail="Username or email already registered")
-    
-    hashed_pw = hash_password(user.password)
-    db_user = User(
-        username= user.username,
-        first_name= user.first_name,
-        middle_name= user.middle_name,
-        last_name=user.last_name,
-        email= user.email,
-        password=hashed_pw    
-    )
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
-
-@app.post("/login")
-def login(user: UserLogin, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.username == user.username).first()
-    if not db_user or not verify_password(user.password, db_user.password):
-        raise HTTPException(status_code=401, detail="Invalid credentials")
-    
-    expire = datetime.now(timezone.utc) + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
-    payload = {
-        "sub": str(db_user.id),
-        "exp": expire
-    }
-    access_token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
-
-    return {"access_token": access_token, "token_type": "bearer"}
-
-@app.post("/logout")
-def logout():
-    return {"logged out"}
-
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
