fc5aa3be0ee811bc0a1c164ba6f412f47d45e61a
cindrellakoju <cindrellakoju@gmail.com>
2025-06-27 12:45:45 +0545

    Managed the folders

--- app/main.py
+++ app/main.py
@@ -1,24 +1,19 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer
+from app.routes import communication,chats,auth
 from routes.auth import router
 
 
 from app.database import SessionLocal, engine
 from app.models import Base
-from app.route import chats, communication
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(chats.router)
 app.include_router(communication.router)
+app.include_router(auth.router)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "/login")
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
