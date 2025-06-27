92e11d3ce9cf0a5b7038c5682df135bea153eac1
cindrellakoju <cindrellakoju@gmail.com>
2025-06-27 12:24:56 +0545

    added the chat crud and multiuser communication with authorization

--- app/main.py
+++ app/main.py
@@ -10,21 +10,23 @@ from app.models import Base, User
 from app.schemas import UserCreate, UserLogin, UserResponse
 from app.utils import hash_password, verify_password
 from app.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
-
+from app.route import chats, communication
+from app.database import get_db
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(chats.router)
+app.include_router(communication.router)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "/login")
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
 
 @app.post("/register", response_model=UserResponse)
---
+++ app/validations.py
@@ -0,0 +1,44 @@
+from fastapi import Header, HTTPException, Depends,Query
+from database.models import User, RoomMembers
+from app.config import SECRET_KEY,ALGORITHM
+import jwt
+from app.database import get_db
+from sqlalchemy.orm import Session
+
+def get_current_user(authorization: str | None = Header(...), db: Session = Depends(get_db)):
+    try:
+        scheme, token = authorization.split()
+        if scheme.lower() != "bearer":
+            raise HTTPException(status_code=401, detail="Invalid token scheme")
+        
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        print(payload)
+        user_id = payload.get("sub")
+        user = db.query(User).filter_by(id=user_id).first()
+        print(user)
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+        return user
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired")
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid or missing token")
+    
+def verify_token(obtained_token: str = Query(...), db : Session = Depends(get_db)):
+    try:
+        token = obtained_token
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = int(payload.get("sub"))
+        print(f"user_id from token: {user_id}")
+        user = db.query(User).filter_by(id = user_id).first()
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+        return user
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired")
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid or missing token")
+    
+def check_user_inroom(userid: int, roomid: int, db: Session):
+    membership = db.query(RoomMembers).filter_by(user_id=userid, room_id=roomid).first()
+    return membership is not None
