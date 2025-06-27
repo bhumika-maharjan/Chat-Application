936f56ef0fe126848760560024b386c7cff758cc
cindrellakoju <cindrellakoju@gmail.com>
2025-06-27 12:37:18 +0545

    Managed conflict

--- app/database.py
+++ app/database.py
@@ -4,10 +4,3 @@ from app.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
