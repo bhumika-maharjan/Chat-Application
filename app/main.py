9937e609e7b2110e36084eecaf0be2a311afce75
cindrellakoju <cindrellakoju@gmail.com>
2025-06-29 13:49:07 +0545

    testing of file transfer

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.routes import communication,chats,auth
 
 from app.database import engine
 from app.models import Base
+from fastapi.staticfiles import StaticFiles
 
 
 Base.metadata.create_all(bind=engine)
@@ -12,5 +13,7 @@ app = FastAPI()
 app.include_router(chats.router)
 app.include_router(communication.router)
 app.include_router(auth.router)
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "/login")
