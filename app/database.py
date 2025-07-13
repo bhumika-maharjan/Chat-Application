f2356b9457fbd9a6be350aa21455bfe1cc4c8f76
Bhumika <bhumika2060@gmail.com>
2025-07-13 20:52:28 +0545

    Save local changes before merge

--- app/database.py
+++ app/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from app.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def get_db():
     db = SessionLocal()
--- app/utils.py
+++ app/utils.py
@@ -1,26 +1,31 @@
-from passlib.context import CryptContext
-from fastapi import WebSocket
-from fastapi import Header, HTTPException, Depends,Query
-from database.models import User, RoomMembers
-from app.config import SECRET_KEY,ALGORITHM
 import jwt
-from app.database import get_db
+from fastapi import Depends, Header, HTTPException, Query
+from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+from app.config import ALGORITHM, SECRET_KEY
+from app.database import get_db
+from database.models import RoomMembers, User
 
-def hash_password(password: str)->str:
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-def verify_password(plain_password:str, hashed_password: str)->bool:
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_current_user(authorization: str | None = Header(...), db: Session = Depends(get_db)):
+
+def get_current_user(
+    authorization: str | None = Header(...), db: Session = Depends(get_db)
+):
     try:
         scheme, token = authorization.split()
         if scheme.lower() != "bearer":
             raise HTTPException(status_code=401, detail="Invalid token scheme")
-        
+
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         print(payload)
         user_id = payload.get("sub")
@@ -33,8 +38,9 @@ def get_current_user(authorization: str | None = Header(...), db: Session = Depe
         raise HTTPException(status_code=401, detail="Token has expired")
     except Exception:
         raise HTTPException(status_code=401, detail="Invalid or missing token")
-    
-def verify_token(obtained_token: str = Query(...), db : Session = Depends(get_db)):
+
+
+def verify_token(obtained_token: str = Query(...), db: Session = Depends(get_db)):
     try:
         if obtained_token.lower().startswith("bearer "):
             token = obtained_token[7:]  # strip first 7 chars (bearer + space)
@@ -43,7 +49,7 @@ def verify_token(obtained_token: str = Query(...), db : Session = Depends(get_db
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         user_id = int(payload.get("sub"))
         print(f"user_id from token: {user_id}")
-        user = db.query(User).filter_by(id = user_id).first()
+        user = db.query(User).filter_by(id=user_id).first()
 
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
@@ -52,11 +58,13 @@ def verify_token(obtained_token: str = Query(...), db : Session = Depends(get_db
         raise HTTPException(status_code=401, detail="Token has expired")
     except Exception:
         raise HTTPException(status_code=401, detail="Invalid or missing token")
-    
+
+
 def check_user_inroom(userid: int, roomid: int, db: Session):
     membership = db.query(RoomMembers).filter_by(user_id=userid, room_id=roomid).first()
     return membership is not None
 
-def verify_user(id : int, db:Session):
-    userexist = db.query(User).filter_by(id = id).first()
+
+def verify_user(id: int, db: Session):
+    userexist = db.query(User).filter_by(id=id).first()
     return userexist is not None
