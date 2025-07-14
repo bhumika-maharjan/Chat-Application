17c769dacbed18bc730b84017ced4902b05ff997
Bhumika <bhumika2060@gmail.com>
2025-07-14 15:30:35 +0545

    migration fixed

--- database/chatapp/versions/17b9b1833494_merge_heads_to_unify_migrations.py
+++ database/chatapp/versions/17b9b1833494_merge_heads_to_unify_migrations.py
@@ -5,17 +5,14 @@ Revises: 5548ced79565, b0459cbd8ae5
 Create Date: 2025-07-14 15:11:11.704098
 
 """
-from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
+from collections.abc import Sequence
 
 # revision identifiers, used by Alembic.
-revision: str = '17b9b1833494'
-down_revision: Union[str, Sequence[str], None] = ('5548ced79565', 'b0459cbd8ae5')
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = "17b9b1833494"
+down_revision: str | Sequence[str] | None = ("5548ced79565", "b0459cbd8ae5")
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
